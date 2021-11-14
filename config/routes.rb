a749bf8f7dbd1540c1651898dd990b638776b7bc
d1mentor <gerasimenkot92@gmail.com>
2021-11-14 20:04:38 +0200

    add controllers, views, and configurate devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/new'
+  get 'users/create'
+  get 'users/destroy'
+  get 'users/delete'
+  get 'users/edit'
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "users#index"
 end
